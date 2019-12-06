f2b98207d34edd0463352d66b39800c3513e0bb1
WALISON PITOMBEIRA DA SILVA <wpitombeira.ipms@gmail.com>
2019-12-06 17:12:14 -0300

    AT05 Updates

--- AT05/includes/graph.h
+++ AT05/includes/graph.h
@@ -6,15 +6,26 @@
 #include <stdbool.h>
 #include <string.h>
 
-typedef struct {
+typedef struct vertice *vPointer;
+
+typedef struct vertice {
   int key;
   char color;
   int d;
   int f;
   int dad;
-  int *adjs;
+  vPointer adjs;
+  int nadj;
 } vertice;
 
-void init(vertice*);
+void init(vertice*, int, int*, int);
+int distinct(int *array, int size);
+void updateAdj(vertice*, int, int*, int, int);
+void newGraph(vertice*, int*, int);
+
+void bfs();
+void dfs(vertice*, int);
+void dfsVisit(vertice*, vertice*, int*);
+
 
 #endif
--- AT05/main.c
+++ AT05/main.c
@@ -1,5 +1,47 @@
 #include "includes/graph.h"
 
+void init(vertice *v, int size, int *numbers, int arrsize){
+  for (int i = 0; i < size; i++) {
+    v[i].color = 'w';
+    v[i].d = 999;
+    v[i].f = 999;
+    v[i].dad = -1;
+    v[i].nadj = 0;
+    updateAdj(v,i,numbers,arrsize,size);
+  }
+}
+
+vertice search(vertice *v, int key, int size){
+  for(int i = 0; i < size; i++){
+    if(v[i].key == key){
+      return v[i];
+    }
+  }
+}
+
+void updateAdj(vertice *v,int index, int *numbers, int size, int graphsize){
+  int iterator=0;
+  vertice *aux;
+  vertice objective;
+  for(int i=0; i < size; i += 2){
+    if(numbers[i] == v[index].key){
+      objective = search(v, numbers[i], graphsize);
+      if(iterator==0){
+        iterator++;
+        aux = malloc(iterator*sizeof(vertice));
+        aux[iterator-1] = objective;
+        v[index].nadj++;
+      } else {
+        iterator++;
+        aux = realloc(aux, iterator*sizeof(vertice));
+        aux[iterator-1] = objective;
+        v[index].nadj++;
+      }
+    }
+  }
+  v[index].adjs = aux;
+}
+
 int distinct(int *array, int size){
   int res = 0;
   for(int i=0; i<size; i++){
@@ -15,6 +57,50 @@ int distinct(int *array, int size){
   return res;
 }
 
+void newGraph(vertice *v, int *array, int size){
+  int res = 0;
+  for(int i=0; i<size; i++){
+    int j = 0;
+    for(j = 0; j<i; j++){
+      if(array[i] == array[j])
+        break;
+    }
+    if(i == j){
+      v[res].key = array[i];
+      res++;
+    }
+  }
+}
+
+void bfs(){
+
+}
+
+void dfs(vertice *v, int size){
+  int time=0;
+  for (int i = 0; i < size; i++) {
+    if(v[i].color = 'w'){
+      dfsVisit(v, &v[i], &time);
+    }
+  }
+}
+
+void dfsVisit(vertice *v, vertice *u, int *time){
+  time += 1;
+  u->d = time;
+  u->color = 'g';
+  for(int j = 0; j < u->nadj; j++){
+    if(u->adjs[j].color == 'w'){
+      u->adjs[j].dad = u->key;
+      dfsVisit(v, &u->adjs[j], time);
+    }
+  }
+  u->color = 'b';
+  time += 1;
+  u->f = time;
+  printf("%d\n", time);
+}
+
 int main(int argc, char **argv){
   if(argc < 3){
     printf("## Inclua os arquivos de entrada e saida\n");
@@ -23,8 +109,9 @@ int main(int argc, char **argv){
     FILE *arqin, *arqout;
     char pmodus, modus, *tok, content[200];
     vertice *vertices;
-    int init, iterator=0, dsts;
-    int *numbers;
+    int initialVertice, dsts;
+    int iterator=0;
+    int *numbers = malloc(sizeof(int));
 
     arqin = fopen(argv[1], "r");
     arqout = fopen(argv[2], "w");
@@ -46,13 +133,14 @@ int main(int argc, char **argv){
     }
 
     dsts =  distinct(numbers, iterator);
-    vertices = malloc(dsts * sizeof(vertices));
+    vertices = malloc(dsts * sizeof(vertice));
+    newGraph(vertices, numbers, iterator);
+    init(vertices,dsts,numbers,iterator);
 
-    for(int i = 0; i < iterator; i++){
-      // printf("%d\n", numbers[i]);
-    }
+    dfs(vertices,dsts);
+
     if(modus == 'B' || modus == 'b'){
-      fscanf(arqin, "%d", &init);
+      fscanf(arqin, "%d", &initialVertice);
     }
   }
 
